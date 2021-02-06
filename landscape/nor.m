e4868398a40a3040cf7b613b0c49cf5f90b407f9
chunhelilab <69711852+chunhelilab@users.noreply.github.com>
2021-02-06 13:16:04 +0800

    Add files via upload

---
+++ landscape/nor.m
@@ -0,0 +1,13 @@
+%normalizing, range 0 to 1
+function SS1= nor(SS)
+% N=23;
+kkkk=size(SS);
+ma=zeros(1,kkkk(2));
+SS1=zeros(kkkk(1),kkkk(2));
+for nnnn=1:kkkk(2)
+    ma(nnnn)=max(SS(:,nnnn));
+    for jjj=1:kkkk(1)
+    SS1(jjj,nnnn)=SS(jjj,nnnn)./ma(nnnn);
+    end
+end
+
